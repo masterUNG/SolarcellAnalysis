19d74ecdaeea534380413f6b9c1305ba7b61f151
ung <test@abc.com>
2022-02-26 15:53:05 +0700

    finish 26feb

--- lib/states/find_apikey.dart
+++ lib/states/find_apikey.dart
@@ -1,14 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:solacellanalysin/utility/my_constant.dart';
+import 'package:solacellanalysin/widgets/show_text.dart';
 
+class FindApiKey extends StatefulWidget {
+  const FindApiKey({Key? key}) : super(key: key);
 
-class FindApiKey extends StatelessWidget {
-  const FindApiKey({ Key? key }) : super(key: key);
+  @override
+  State<FindApiKey> createState() => _FindApiKeyState();
+}
+
+class _FindApiKeyState extends State<FindApiKey> {
+  String? siteId, apiKey;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    siteId = MyConstant.siteId;
+    apiKey = MyConstant.apiKey;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Text('This is Find API key'),
+      body: Column(
+        children: [
+          ShowText(label: 'siteId = $siteId'),
+          ShowText(label: 'apiKey = $apiKey'),
+        ],
+      ),
     );
   }
 }
--- lib/utility/my_constant.dart
+++ lib/utility/my_constant.dart
@@ -3,4 +3,7 @@ import 'package:flutter/material.dart';
 class MyConstant {
   static String routeMainHome = '/mainHome';
   static String routeFindAPIkey = '/findApiKey';
+
+  static String apiKey = 'VVOX8PCKBXGAHY3E2HKVJLTHDWSZH81M';
+  static String siteId = '1598054';
 }
